c1c004e65dd6a80401339435fcb7d3c1b8881c4a
vanpine <123456@qq.com>
2024-07-01 20:29:14 +0800

    项目启动模版

---
+++ src/main/java/com/vanpine/VanpineTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.vanpine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanpineTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanpineTemplateApplication.class, args);
+    }
+
+}
